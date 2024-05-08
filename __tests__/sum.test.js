6b9da7d1b9dd3ffe85ff2c766acddeafd888105b
michellehuang728 <michellely.huang728@gmail.com>
2024-05-08 10:36:16 -0700

    commit tobe3

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -10,8 +10,8 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1,2)).toBe(3);
 });
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(3);
-});
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1,2)).toBe(3);
+// });
 
 
